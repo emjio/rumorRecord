580ba7d2e8e2c807131d9cfa69528e631e61e773
emjio <1271246683@qq.com>
2020-01-29 13:08:25 +0800

    init cli

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rumorRecord
 2020年SARS病毒传播期间谣言记录和物价哄抬记录。
+
+启动 
+npm run serve
---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>asdsa</div>
+</template>
+
+<script lang="ts">
+import { Vue,Components } from "vue-property-decorator";
+@Components
+export default class Index extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
